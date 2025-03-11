b207dcade5dc308cc3129e6f93733ae90648fe39
MyskYko <yoyuohlhjl@yahoo.co.jp>
2025-03-10 18:12:10 -0700

    update

--- main.cpp
+++ main.cpp
@@ -12,94 +12,43 @@
 #include <argparse/argparse.hpp>
 
 #include <chrono>
+#include <string>
+#include <random>
 
 #include "alias.h"
 
+#define AIGSIZE Abc_NtkNodeNum(Abc_FrameReadNtk(pAbc))
+#define GIASIZE Gia_ManAndNum(Abc_FrameReadGia(pAbc))
+
 using namespace std;
 
-string abcopt = "&dc2";
-int tseed;
-bool fCspf;
-bool fMerge;
-Gia_Man_t * opt1(Gia_Man_t * pOld) {
-  Abc_Frame_t * pAbc = Abc_FrameGetGlobalFrame();
-  Gia_Man_t * pGia = Gia_ManDup(pOld);
-  int n;
-  do {
-    n = Gia_ManAndNum(pGia);
-    Gia_Man_t * pNew = Gia_ManTransduction(pGia, (fMerge? 6: 7), !fCspf, tseed++, 0, 0, 0, NULL, 0);
-    Gia_ManStop(pGia);
-    pAbc->pGia = pNew;    
-    Cmd_CommandExecute(pAbc, abcopt.c_str());
-    pGia = pAbc->pGia;
-    pAbc->pGia = NULL;
-    cout << "\t\t\topt1 " << Gia_ManAndNum(pGia) << endl;
-  } while(n > Gia_ManAndNum(pGia));
-  return pGia;
-}
-
-string abchop = "&put; if; mfs2; st; &get;";
-int nhops;
-bool resethop;
-Gia_Man_t * opt2(Gia_Man_t * pOld) {
-  Abc_Frame_t * pAbc = Abc_FrameGetGlobalFrame();
-  Gia_Man_t * pGia = Gia_ManDup(pOld);
-  int n = Gia_ManAndNum(pGia);
-  Gia_Man_t * pBest = Gia_ManDup(pGia);
-  for(int i = 0; i <= nhops; i++) {
-    Gia_Man_t * pNew = opt1(pGia);
-    cout << "\t\topt2 " << Gia_ManAndNum(pNew) << endl;
-    Gia_ManStop(pGia);
-    if(n > Gia_ManAndNum(pNew)) {
-      n = Gia_ManAndNum(pNew);
-      Gia_ManStop(pBest);
-      pBest = Gia_ManDup(pNew);
-      if(resethop) {
-        i = 0;
-      }
-    }
-    pAbc->pGia = pNew;
-    Cmd_CommandExecute(pAbc, abchop.c_str());
-    pGia = pAbc->pGia;
-    pAbc->pGia = NULL;
-  }
-  Gia_ManStop(pGia);
-  return pBest;
-}
-
-int nrestarts;
-int seedbase;
-Gia_Man_t * opt3(Gia_Man_t * pOld) {
-  int n = Gia_ManAndNum(pOld);
-  Gia_Man_t * pBest = Gia_ManDup(pOld);
-  for(int i = 0; i <= nrestarts; i++) {
-    tseed = 1234 * (i + seedbase);
-    Gia_Man_t * pNew = opt2(pOld);
-    cout << "\topt3 " << Gia_ManAndNum(pNew) << endl;
-    if(n > Gia_ManAndNum(pNew)) {
-      n = Gia_ManAndNum(pNew);
-      Gia_ManStop(pBest);
-      pBest = pNew;
-    } else {
-      Gia_ManStop(pNew);
-    }
-  }
-  return pBest;
+std::string DeepSynOne(int i) {
+  int fUseTwo = 0;
+  unsigned Rand = Abc_Random(0);
+  int fDch = Rand & 1;
+  //int fCom = (Rand >> 1) & 3;
+  //int fCom = (Rand >> 1) & 1;
+  int fFx  = (Rand >> 2) & 1;
+  int KLut = fUseTwo ? 2 + (i % 5) : 3 + (i % 4);
+  std::string Command;
+  Command += "&dch";
+  Command += fDch ? " -f" : "";
+  Command += "; ";
+  Command += "&if -a -K ";
+  Command += std::to_string(KLut);
+  Command += "; ";
+  Command += "&mfs -e -W 20 -L 20; ";
+  Command += fFx ? "&fx; &st" : "";
+  return Command;
 }
 
 int main(int argc, char **argv) {
+  // argparse
   argparse::ArgumentParser ap("opt");
   ap.add_argument("input");
-  ap.add_argument("output");
-  ap.add_argument("-n", "--num_restarts").default_value(0).scan<'i', int>();
-  ap.add_argument("-m", "--num_hops").default_value(0).scan<'i', int>();
-  ap.add_argument("-r", "--reset_hop").default_value(true).implicit_value(false);
-  ap.add_argument("-b", "--seed_base").default_value(0).scan<'i', int>();
-  ap.add_argument("-c", "--cspf").default_value(false).implicit_value(true);
-  ap.add_argument("-s", "--multi_starts").default_value(true).implicit_value(false);
-  ap.add_argument("-t", "--original_only").default_value(false).implicit_value(true);
-  ap.add_argument("-u", "--merge").default_value(true).implicit_value(false);
-  ap.add_argument("-v", "--verbose").default_value(false).implicit_value(true);
+  ap.add_argument("-o", "--output");
+  ap.add_argument("-m", "--max_noimp").default_value(10).scan<'i', int>();
+  ap.add_argument("-r", "--random_seed").default_value(0).scan<'i', int>();
   try {
     ap.parse_args(argc, argv);
   }
@@ -109,102 +58,110 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // set argument
-  nhops = ap.get<int>("--num_hops");
-  nrestarts = ap.get<int>("--num_restarts");
-  resethop = ap.get<bool>("--reset_hop");
-  seedbase = ap.get<int>("--seed_base");
-  fCspf = ap.get<bool>("--cspf");
-  fMerge = ap.get<bool>("--merge");
-  //vector<string> abccmds = {"resyn;", "resyn2;", "resyn2a;", "resyn3;", "compress;", "compress2;", "resyn2rs;", "compress2rs;", "resub –l -N 2 -K 16;", "iresyn –l;", "&get;&fraig –x;&put;"};
+  // parameters
+  int max_noimp = ap.get<int>("-m");
+  int random_seed = ap.get<int>("-m");
+  std::mt19937 rng(random_seed);
 
   // abc init
-  auto start = chrono::steady_clock::now();
   Abc_Start();
   Abc_Frame_t * pAbc = Abc_FrameGetGlobalFrame();
+  Abc_Random(1);
+  for(int s = 0; s < 10 + random_seed; s++) {
+    Abc_Random(0);
+  }
   
   // setup alias
   for(char * cmd: alias) {
     Cmd_CommandExecute(pAbc, cmd);
   }
+  //vector<string> abccmds = {"resyn;", "resyn2;", "resyn2a;", "resyn3;", "compress;", "compress2;", "resyn2rs;", "compress2rs;", "resub –l -N 2 -K 16;", "iresyn –l;", "&get;&fraig –x;&put;"};
 
   // read aig
-  Gia_Man_t * pGia;
   {
     string str = ap.get<string>("input");
-    char * cstr = new char[str.length() + 1];
-    strcpy(cstr, str.c_str());
-    pGia = Gia_AigerRead(cstr, 0, 0, 1);
+    char * cstr = new char[6 + str.length() + 1];
+    strcpy(cstr, "&read ");
+    strcat(cstr, str.c_str());
+    Cmd_CommandExecute(pAbc, cstr);
     delete [] cstr;
   }
-
-  // setup start points
-  vector<Gia_Man_t *> start_points;
-  start_points.push_back(Gia_ManDup(pGia));
-  if(!ap.get<bool>("--original_only")) {
-    {
-      pAbc->pGia = Gia_ManDup(pGia);
-      Cmd_CommandExecute(pAbc, "&put; collapse; strash; &get");
-      start_points.push_back(Gia_ManDup(pAbc->pGia));
-      Gia_ManStop(pAbc->pGia);
-      pAbc->pGia = NULL;
-    }
-    {
-      Gia_Man_t * pNew = Gia_ManTtopt(pGia, Gia_ManCiNum(pGia), Gia_ManCoNum(pGia), 100);
-      start_points.push_back(pNew);
-    }
-    {
-      pAbc->pGia = Gia_ManDup(pGia);
-      Cmd_CommandExecute(pAbc, "&put; collapse; sop; fx; strash; &get");
-      start_points.push_back(Gia_ManDup(pAbc->pGia));
-      Gia_ManStop(pAbc->pGia);
-      pAbc->pGia = NULL;
-    }
-    //Cmd_CommandExecute(pAbc, "&put; collapse; dsd; sop; fx; strash; &get");
-  }
-
-  if(!ap.get<bool>("--multi_starts")) {
-    Gia_Man_t * pBest = Gia_ManDup(pGia);
-    for(auto p: start_points) {
-      if(Gia_ManAndNum(p) < Gia_ManAndNum(pBest)) {
-        Gia_ManStop(pBest);
-        pBest = Gia_ManDup(p);
-      }
-      Gia_ManStop(p);
-    }
-    start_points.clear();
-    start_points.push_back(pBest);
-  }
+  cout << "init: " << GIASIZE << endl;
 
   // optimize
-  for(auto p: start_points) {
-    cout << "init " << Gia_ManAndNum(p) << endl;
-    Gia_Man_t * pNew = opt3(p);
-    cout << "end " << Gia_ManAndNum(pNew) << endl;
-    if(Gia_ManAndNum(pGia) > Gia_ManAndNum(pNew)) {
-      Gia_ManStop(pGia);
-      pGia = pNew;
+  auto start = chrono::steady_clock::now();
+  Gia_Man_t * pGia = Gia_ManDup(Abc_FrameReadGia(pAbc));
+  int n = GIASIZE;
+  //int n = AIGSIZE;
+  int itr = 0;
+  int itr_end = itr + max_noimp;
+  while(itr < itr_end) {
+    // ifmfs
+    std::string Command = DeepSynOne(itr);
+    cout << Command << endl;
+    Cmd_CommandExecute(pAbc, Command.c_str());
+    cout << "ifmfs: " << GIASIZE << endl;
+
+    if(rng() & 1) {
+      Cmd_CommandExecute(pAbc, "&put");
+      int m = GIASIZE;
+      Cmd_CommandExecute(pAbc, "compress2rs");
+      cout << "c2rs: " << setw(5) << AIGSIZE << endl;
+      while(m > AIGSIZE) {
+        m = AIGSIZE;
+        Cmd_CommandExecute(pAbc, "compress2rs");
+        cout << "c2rs: " << setw(5) << AIGSIZE << endl;
+      }
+      Cmd_CommandExecute(pAbc, "&get");
     } else {
-      Gia_ManStop(pNew);
+      int m = ABC_INT_MAX;
+      while(m > GIASIZE) {
+        m = GIASIZE;
+        Cmd_CommandExecute(pAbc, "&dc2");
+        cout << "dc2: " << setw(5) <<  GIASIZE << endl;
+      }
     }
-    Gia_ManStop(p);
+    //Cmd_CommandExecute(pAbc, "compress2rs");
+    //Cmd_CommandExecute(pAbc, "orchestrate -K 12 -N 2 -l");
+    //Cmd_CommandExecute(pAbc, "orchestrate -K 12 -N 2 -l; balance");
+    //Cmd_CommandExecute(pAbc, "if -K 6; mfs2; fx; st; compress2rs");
+    
+    itr++;
+    if(n > GIASIZE) {
+      n = GIASIZE;
+      itr_end = itr + max_noimp;
+      Gia_ManStop(pGia);
+      pGia = Gia_ManDup(Abc_FrameReadGia(pAbc));
+    }
+    cout << itr << " " << Abc_NtkNodeNum(Abc_FrameReadNtk(pAbc)) << endl;
+    /*
+    if(n > Abc_NtkNodeNum(Abc_FrameReadNtk(pAbc))) {
+      n = Abc_NtkNodeNum(Abc_FrameReadNtk(pAbc));
+      itr_end = itr + max_noimp;
+      //pNtkRes = Abc_NtkDup( pNtk );
+    }
+    */
   }
+  cout << endl;
+    
   auto end = chrono::steady_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
   cout << "elapsed_seconds " << elapsed_seconds.count() << endl;
-  cout << "best " << Gia_ManAndNum(pGia) << endl;
+  cout << "iteration " << itr << endl;
+  cout << "size " << n << endl;
 
   // write
-  {
-    string str = ap.get<string>("output");
-    char * cstr = new char[str.length() + 1];
-    strcpy(cstr, str.c_str());
-    Gia_AigerWrite(pGia, cstr, 0, 0, 0);
+  if(auto poutput = ap.present("-o")) {
+    Abc_FrameUpdateGia(pAbc, pGia);
+    string str = *poutput;
+    char * cstr = new char[7 + str.length() + 1];
+    strcpy(cstr, "&write ");
+    strcat(cstr, str.c_str());
+    Cmd_CommandExecute(pAbc, cstr);
     delete [] cstr;
   }
 
   // finish
-  Gia_ManStop(pGia);
   Abc_Stop();
   
   return 0;
